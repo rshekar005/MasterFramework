27ccb1e26d1fcf79cb0164919cfeb123c09e3c66
rshekar005 <rshekar005@gmail.com>
2022-11-07 23:09:19 +0530

    changed the path and updated classes

---
+++ src/main/java/com/tmb/driver/web/local/LocalDriverManager.java
@@ -0,0 +1,36 @@
+package com.tmb.driver.web.local;
+
+import com.tmb.driver.manager.web.local.ChromeManager;
+import com.tmb.driver.manager.web.local.FirefoxManager;
+import com.tmb.enums.BrowserType;
+import org.openqa.selenium.WebDriver;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public final class LocalDriverManager {
+    private LocalDriverManager() {
+    }
+
+    private static final Map<BrowserType, Supplier<WebDriver>> MAP = new EnumMap<BrowserType, Supplier<WebDriver>>(BrowserType.class);
+    private static final Supplier<WebDriver> CHROME = ChromeManager::getDriver;
+    private static final Supplier<WebDriver> FIREFOX = FirefoxManager::getDriver;
+
+    static {
+        MAP.put(BrowserType.CHROME, CHROME);
+        MAP.put(BrowserType.FIREFOX, FIREFOX);
+    }
+
+    public static WebDriver getDriver(BrowserType browserType) {
+        return MAP.get(browserType).get();
+    }
+   /* Without lambda
+   public static WebDriver getDriver(BrowserType browserType) {
+        return isChromeBrowser(browserType) ? ChromeManager.getDriver() : FirefoxManager.getDriver();
+    }
+
+    private static boolean isChromeBrowser(BrowserType browserType) {
+        return browserType == BrowserType.CHROME;
+    }*/
+}
---
+++ src/main/java/com/tmb/driver/web/remote/RemoteDriverFactory.java
@@ -0,0 +1,42 @@
+package com.tmb.driver.web.remote;
+
+import com.tmb.driver.web.remote.SeleniumGridFactory;
+import com.tmb.enums.BrowserRemoteMode;
+import com.tmb.enums.BrowserType;
+import org.openqa.selenium.WebDriver;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public final class RemoteDriverFactory {
+    private RemoteDriverFactory() {
+    }
+
+    private static final Map<BrowserRemoteMode, Function<BrowserType, WebDriver>> MAP = new EnumMap<>(BrowserRemoteMode.class);
+    private static final Function<BrowserType, WebDriver> SELENIUM = SeleniumGridFactory::getDriver;
+    private static final Function<BrowserType, WebDriver> SELENOID = SelenoidFactory::getDriver;
+    private static final Function<BrowserType, WebDriver> BROWSER_STACK = BrowserstackFactory::getDriver;
+
+    static {
+        MAP.put(BrowserRemoteMode.SELENIUM, SELENIUM);
+        MAP.put(BrowserRemoteMode.SELENOID, SELENOID);
+        MAP.put(BrowserRemoteMode.BROWSER_STACK, BROWSER_STACK);
+    }
+
+    public static WebDriver getDriver(BrowserRemoteMode browserRemoteMode, BrowserType browserType) {
+        return MAP.get(browserRemoteMode).apply(browserType);
+    }
+    /* Traditional Way
+    public static WebDriver getDriver(BrowserRemoteMode browserRemoteMode, BrowserType browserType) {
+        if (browserRemoteMode == BrowserRemoteMode.SELENIUM) {
+            return SeleniumGridFactory.getDriver(browserType);
+        } else if (browserRemoteMode == BrowserRemoteMode.BROWSER_STACK) {
+            return SelenoidFactory.getDriver(browserType);
+        } else if (browserRemoteMode == BrowserRemoteMode.SELENOID) {
+        return BrowserstackFactory.getDriver(browserType);
+        }
+        return null;
+    }*/
+
+}
